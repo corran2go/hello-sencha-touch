17ea8e3c4e3da47f8ae3fc18c7ff0611bef2107b
Steffen Werner <steffen.werner@inovex.de>
2012-03-06 09:40:34 +0100

    tuned layout test

--- app/view/Layout.js
+++ app/view/Layout.js
@@ -11,13 +11,37 @@ Ext.define('App.view.Layout', {
     items: [
       {
         flex: 1,
-        html: 'Left Panel, 1/3rd of total size',
-        style: 'background-color: #5E99CC;'
+        layout: 'vbox',
+        style: 'background-color: #5E99CC;',
+
+        items: [
+          {
+            flex: 1,
+            html: 'Left Panel, half width, half height',
+          },
+          {
+            flex: 1,
+            html: 'Left Bottom Panel, half height',
+            style: 'background-color: #6FAADD;'
+          }
+        ]
       },
       {
-        flex: 2,
-        html: 'Right Panel, 2/3rds of total size',
-        style: 'background-color: #759E60;'
+        flex: 1,
+        layout: 'vbox',
+        style: 'background-color: #759E60;',
+
+        items: [
+          {
+            flex: 1,
+            html: 'Right Panel, half width, half height'
+          },
+          {
+            flex: 1,
+            html: 'Right Bottom Panel, half height',
+            style: 'background-color: #86AF71;'
+          }
+        ]
       }
     ]
   }
